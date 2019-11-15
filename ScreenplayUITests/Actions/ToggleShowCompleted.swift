cfae63ac14a25e15e04e4f2a4f4d1bbd454f6357
Xiaoxing Hu <xiaoxing@huxx.org>
2019-11-15 21:42:22 +1300

    it's working now

--- ScreenplayUITests/Actions/ToggleShowCompleted.swift
+++ ScreenplayUITests/Actions/ToggleShowCompleted.swift
@@ -2,9 +2,6 @@ import Foundation
 
 struct ToggleShowCompleted: Actionable {
     func perform(_ state: inout AppState) {
-
-        // This doesn't work, and it's not my fault
         TodoListElements.showCompletedToggle(state).tap()
-
     }
 }
